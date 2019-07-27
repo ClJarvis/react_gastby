97c00bd1e73ce628df37a52c8c8a4f1f16dde771
Chris <clj@ollure.com>
2019-07-27 14:14:43 -0500

    adding new components

--- tango-gatsby/src/components/layout.js
+++ tango-gatsby/src/components/layout.js
@@ -1,52 +1,44 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React, { useState} from 'react;'
+import PropTypes from 'prop-types';
+import sytled from 'sytled-components';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import Header from './Header';
+import OverlayMenu from './OverlayMenu';
+import Hamburger from '/Hamburger';
+import Footer from '.Footer';
 
-import Header from "./header"
-import "./layout.css"
+import './layout.css';
+import 'bootstrap/dist/css/Bootstrap-grid.css';
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+const Primary = sytled.main`
+  padding: 110px 0 0 0;
+`
 
+const layout = ({ children}) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleOverlayMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Hamburger />
+      <OverlayMenu />
+      <Header />
+      <Primary id="primary" classname="content-area">
+        <main id="main" classname="site-main" role="main">
+          {children}
+        </main>
+      </Primary>
+      <Footer />
     </>
-  )
+    )
 }
 
-Layout.propTypes = {
+layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default layout;
+
+
